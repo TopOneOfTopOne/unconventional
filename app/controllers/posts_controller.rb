02d6f89a961a050f24a13b998649bdaf8563ac49
JD Crouch <jdcrouch5@gmail.com>
2016-03-25 23:15:34 +0900

    Add Edit, Update, and Destroy actions to PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,36 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.assign_attributes(post_params)
+
+
+    if @post.save!
+      flash[:notice] = "Post was saved"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was a problem saving the post"
+      render :edit
+    end
+  end
+
+  def destroy
+  @post = Post.find(params[:id])
+
+  if @post.destroy
+    flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+    redirect_to posts_path
+  else
+    flash.now[:alert] = "There was an error deleting the post."
+    render :show
+  end
+end
+
   def post_params
     params.require(:post).permit(:title, :description, :picture, :picture_cache)
   end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -54,4 +54,61 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, {id: my_post.id}
+      expect(response).to render_template :edit
+    end
+
+    it "assigns post to be updated to @post" do
+      get :edit, {id: my_post.id}
+
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.description).to eq my_post.description
+      #expect(post_instance.picture).to eq my_post.picture
+    end
+  end
+
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = "Updated Title"
+      new_desc = "This is the new and updated description"
+
+      put :update, id: my_post.id, post: {title: new_title, description: new_desc}
+
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.description).to eq new_desc
+    end
+
+    it "redirects to the updated post" do
+      new_title = "Updated Title"
+      new_desc = "This is the new and updated description"
+
+      put :update, id: my_post.id, post: {title: new_title, description: new_desc}
+      expect(response).to redirect_to my_post
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the post" do
+      delete :destroy, {id: my_post.id}
+      count = Post.where({id: my_post.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to posts index" do
+      delete :destroy, {id: my_post.id}
+      expect(response).to redirect_to posts_path
+    end
+  end
 end

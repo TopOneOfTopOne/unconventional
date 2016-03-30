75b4aa1737db357ccc4acec576511fcd87973ac0
JD Crouch <jdcrouch5@gmail.com>
2016-03-30 19:37:57 +0900

    unfinished jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,5 +19,29 @@
 $(document).ready(function(){
   $('p a').embedly({
     key: '7918a7b91ef74f3790e4e9d15e0f7aa2'
-  })
-})
+  });
+
+  $('#description').on('keyup', function(){
+    console.log("keyup");
+    var message = $('<span>Type in the URL of the site you would like to embed an image from.</span>');
+    if($('#post_description').val().length > 0){
+      console.log($('#post_description').val().length > 0);
+      $('#post_picture').hide();
+      $('#message').html(message)
+    } else {
+      $('#post_picture').show();
+    }
+  });
+
+  $('#picture').on('change', function(){
+    console.log($('#post_picture').value);
+    var message = $('<span>This image will be uploaded to your gallery.</span>');
+    if($('#post_picture').value === true ){
+      console.log($('#post_picture').name);
+      $('#post_description').disabled = true;
+      $(this).append(message)
+    } else {
+      $('#post_picture').disabled = false;
+    }
+  });
+});
